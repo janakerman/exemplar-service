38bf058e39c9831ff7c8bc9f7411b716af687ee2
Jan <j.f.akerman@gmail.com>
2019-04-20 07:33:13 +0100

    Initial commit from Spring Initializer
    .
    
    https://start.spring.io/

---
+++ src/main/java/com/janakerman/exemplarservice/ExemplarServiceApplication.java
@@ -0,0 +1,13 @@
+package com.janakerman.exemplarservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemplarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemplarServiceApplication.class, args);
+	}
+
+}
